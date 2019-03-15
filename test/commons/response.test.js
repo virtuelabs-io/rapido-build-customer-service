03336c415eadc6fbffedb8ff2e500cf3ede8042a
Sangram Reddy <reddy.horcrux@gmail.com>
2019-03-15 21:44:48 +0000

    Confirm registration api added

--- test/commons/response.test.js
+++ test/commons/response.test.js
@@ -18,4 +18,11 @@ describe(`Response handler tests`, () => {
     expect(response.headers["Content-Type"]).toBe("application/json");
   });
 
+  test(`GET Success response`, () => {
+    var response = responseHandler.helper.get("200");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe('\"200\"');
+    expect(response.headers["Content-Type"]).toBe("application/json");
+  });
+
 });
