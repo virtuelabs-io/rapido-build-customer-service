2f25bc8b6db8f7a3518939b343243d2fd0fc2863
Sangram <reddy.horcrux@gmail.com>
2019-04-07 19:16:35 +0100

    Added subnet support

--- src/customer/getCustomerCompanyDetails.js
+++ src/customer/getCustomerCompanyDetails.js
@@ -1,25 +1,18 @@
 'use strict';
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    port: process.env.PORT,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+const mysql = require('serverless-mysql')({
+    config: {
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        port: process.env.PORT,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    }
+  })
 
 module.exports.fun = async (event, context, callback) => {
     global.fetch = require('node-fetch');
-    context.callbackWaitsForEmptyEventLoop = false;
-    connection.query('SELECT * FROM `mysql`.`db` LIMIT 1000;', function (error, results) {
-        if (error) {
-            connection.destroy();
-            throw error;
-        } else {
-            console.log(results);
-            callback(error, results);
-            connection.end(function (err) { callback(err, results);});
-        }
-    });
+    let results = await mysql.query('SELECT * FROM `mysql`.`db` LIMIT 1000;')
+    await mysql.end()
+    return results
 }
